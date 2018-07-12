d9e02a9befdcedcd18c7ec5e447a214894f64fd1
osboxes.org <jmalviz123@gmail.com>
2018-07-12 04:12:41 -0400

    Order of stations fixed

--- puv/views.py
+++ puv/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 
 def home(request):
-    context = {"BACLARAN":"light", "EDSA":"heavy", "LIBERTAD":"light", "GIL PUYAT":"moderate", "VITO CRUZ":"light", "QUIRINO":"light", "PEDRO GIL":"heavy", "BACLARAN":"light", "UN AVENUE":"moderate", "CENTRAL TERMINAL":"light",
-    "CARRIEDO":"light", "DOROTEO JOSE":"heavy", "BAMBANG":"moderate", "TAYUMAN":"light", "BLUMENTRITT":"light", "ABAD SANTOS":"moderate", "BACLARAN":"light", "R.PAPA":"heavy", "5TH AVENUE":"heavy", "MONUMENTO":"light", "MALVAR":"moderate", "BALINTAWAK":"moderate",
-    "ROOSEVELT":"light"}
+
+    context = [('BACLARAN', 'light'), ('EDSA', 'heavy'), ('LIBERTAD', 'light'), ('GIL PUYAT', 'moderate'), ('VITO CRUZ', 'light'), ('QUIRINO', 'light'), ('PEDRO GIL', 'heavy'), ('UN AVENUE', 'moderate'), ('CENTRAL TERMINAL', 'light'),
+    ('CARRIEDO', 'light'), ('DOROTEO JOSE', 'heavy'), ('BAMBANG', 'moderate'), ('TAYUMAN', 'light'), ('BLUMENTRITT', 'light'), ('ABAD SANTOS', 'moderate'), ('R.PAPA', 'heavy'), ('5TH AVENUE', 'heavy'), ('MONUMENTO', 'light'),
+    ('MALVAR', 'moderate'), ('BALINTAWAK', 'moderate'), ('ROOSEVELT', 'light')]
+
+    context.reverse()
 
     return render(request, 'puv/index.html', {'data' : context})
