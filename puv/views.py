888dec441e0c453616adaeffbbe488f7e345c778
osboxes.org <jmalviz123@gmail.com>
2018-07-12 02:10:37 -0400

    Created Dynamic Interfacefrom dummy data

--- puv/views.py
+++ puv/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'puv/index.html', {})
+    context = {"BACLARAN":"light", "EDSA":"heavy", "LIBERTAD":"light", "GIL PUYAT":"moderate", "VITO CRUZ":"light", "QUIRINO":"light", "PEDRO GIL":"heavy", "BACLARAN":"light", "UN AVENUE":"moderate", "CENTRAL TERMINAL":"light",
+    "CARRIEDO":"light", "DOROTEO JOSE":"heavy", "BAMBANG":"moderate", "TAYUMAN":"light", "BLUMENTRITT":"light", "ABAD SANTOS":"moderate", "BACLARAN":"light", "R.PAPA":"heavy", "5TH AVENUE":"heavy", "MONUMENTO":"light", "MALVAR":"moderate", "BALINTAWAK":"moderate",
+    "ROOSEVELT":"light"}
+
+    return render(request, 'puv/index.html', {'data' : context})
