13d079a5185b2f65feab13117b98e22f8ac4ad28
osboxes.org <jmalviz123@gmail.com>
2018-07-13 09:06:03 -0400

    Added Dynamic Gray Scale Background Feature on User Interface

--- puv/views.py
+++ puv/views.py
@@ -2,10 +2,27 @@ from django.shortcuts import render
 
 def home(request):
 
-    context = [('BACLARAN', 'light'), ('EDSA', 'heavy'), ('LIBERTAD', 'light'), ('GIL PUYAT', 'moderate'), ('VITO CRUZ', 'light'), ('QUIRINO', 'light'), ('PEDRO GIL', 'heavy'), ('UN AVENUE', 'moderate'), ('CENTRAL TERMINAL', 'light'),
-    ('CARRIEDO', 'light'), ('DOROTEO JOSE', 'heavy'), ('BAMBANG', 'moderate'), ('TAYUMAN', 'light'), ('BLUMENTRITT', 'light'), ('ABAD SANTOS', 'moderate'), ('R.PAPA', 'heavy'), ('5TH AVENUE', 'heavy'), ('MONUMENTO', 'light'),
+    context = [('BACLARAN', 'light'), ('EDSA', 'heavy'), ('LIBERTAD', 'light'), ('GIL-PUYAT', 'moderate'), ('VITO-CRUZ', 'light'), ('QUIRINO', 'light'), ('PEDRO-GIL', 'heavy'), ('UN-AVENUE', 'moderate'), ('CENTRAL-TERMINAL', 'light'),
+    ('CARRIEDO', 'light'), ('DOROTEO-JOSE', 'heavy'), ('BAMBANG', 'moderate'), ('TAYUMAN', 'light'), ('BLUMENTRITT', 'light'), ('ABAD-SANTOS', 'moderate'), ('R-PAPA', 'heavy'), ('5TH-AVENUE', 'heavy'), ('MONUMENTO', 'light'),
     ('MALVAR', 'moderate'), ('BALINTAWAK', 'moderate'), ('ROOSEVELT', 'light')]
 
     context.reverse()
 
-    return render(request, 'puv/index.html', {'data' : context})
+    currentStation = 'CARRIEDO'
+
+    targetNumOfCurrentStation = getNumberOfStation(currentStation, context)
+
+
+    return render(request, 'puv/index.html', {'data' : context, 'currentStation' : currentStation, 'numOfCurrentStation' : targetNumOfCurrentStation})
+
+def getNumberOfStation(currentStation, context):
+
+    targetNumOfCurrentStation = 1
+
+    for i in context:
+        if i[0] == currentStation:
+            break
+        else:
+            targetNumOfCurrentStation = targetNumOfCurrentStation + 1
+
+    return targetNumOfCurrentStation
