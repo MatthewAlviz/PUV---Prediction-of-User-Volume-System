661581507e1a91d66cef7653821283a7d222b25b
Michael Angelo Cadavillo <michael.cadavillo@gmail.com>
2018-07-15 04:32:10 +0800

    added eventsource feature

--- puv/urls.py
+++ puv/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-    url(r'^jsonData/$', views.getJSONData, name='getJSONData')
+    url(r'^jsonData/$', views.getJSONData, name='getJSONData'),
+    url(r'^jsonLiveData/$', views.getLiveJSONData, name='getLiveJSONData'),
+	url(r'^sse/$', views.sse, name='sse')
 ]
--- puv/views.py
+++ puv/views.py
@@ -3,6 +3,51 @@ import pyrebase
 import datetime
 from django.http import JsonResponse
 from django.http import HttpResponse
+import threading
+from django.http.response import StreamingHttpResponse
+import json
+import time
+
+def movePassengersToTrain():
+    #time.sleep(60)
+    db = connectToDB()
+
+    station = getStationCapacity(db) # number of person in the station
+    train = getTrainCapacity(db)
+    maxTrainCapacity = 10
+    currentPassTrain = 0
+
+    print("stationCapacity: " + str(maxTrainCapacity - station))
+    print("trainCapacity: "  + str(train + station))
+
+    if(station > maxTrainCapacity):
+        setStationCapacity(db, str(maxTrainCapacity - station))
+    else:
+        setStationCapacity(db, str(station - maxTrainCapacity))
+    
+    setTrainCapacity(db, str(train + station))
+
+
+def setTrainCapacity(db, value):
+    #values
+    date = datetime.datetime.today().strftime('%Y-%m-%d')
+    trainID = "1234"
+
+    print("train value: " + value)
+
+    #retrieves data
+    data = db.child(date).child("trainCap").child(trainID).update({"capacity": int(value)})
+
+def setStationCapacity(db, value):
+    #values
+    date = datetime.datetime.today().strftime('%Y-%m-%d')
+    stationID = "0"
+    stationName = "Baclaran"
+
+    print("station value: " + value)
+
+    #retrieves data
+    data = db.child(date).child("stationCap").child(stationID + " - " + stationName).update({"capacity": int(value)})
 
 def connectToDB():
     config = {
@@ -24,7 +69,6 @@ def retrieveData(db):
     station = "0 - Baclaran"
     time = "13:45 - 13:59"
 
-
     #retrieves data
     data = db.child("2018-07-12").child("Entry").child(station).child(time).child("users").get()
     print(dict(data.val()))
@@ -32,22 +76,83 @@ def retrieveData(db):
     return dict(data.val())
     #print("Data: " +  str(data.val()))
 
-def dataListener(message):
+def getStationCapacity(db):
+    #values
+    date = datetime.datetime.today().strftime('%Y-%m-%d')
+    stationID = "0"
+    stationName = "Baclaran"
+
+    #retrieves data
+    data = db.child(date).child("stationCap").child(stationID + " - " + stationName).child("capacity").get()
+    return int(data.val())
+
+def getTrainCapacity(db):
+    #values
+    date = datetime.datetime.today().strftime('%Y-%m-%d')
+    trainID = "1234"
+
+    #retrieves data
+    data = db.child(date).child("trainCap").child(trainID).child("capacity").get()
+
+    return int(data.val())
+
+def retrieveAllData(db):
+    #values
+    date = datetime.datetime.today().strftime('%Y-%m-%d')
+
+    #retrieves data
+    data = db.child(date).get()
+    #print(dict(data.val()))
+
+    return dict(data.val())
+
+def stream_handler(message):
     #print(message["event"]) # put
     #print(message["path"]) # /-K7yGTTEp7O549EzTYtI
     #print("asdf " + str(message["data"])) # {'title': 'Pyrebase', "body": "etc..."}
-    return message["data"]
+    jsonData = message["data"]
+    #print(str(message["data"]))
 
-jsonData = {}
+db = connectToDB()
 
 def getJSONData(request):
-    db = connectToDB()
     data = retrieveData(db)
 
-    date = datetime.datetime.today().strftime('%Y-%m-%d')
-    my_stream = db.child(date).stream(dataListener) 
+    #date = datetime.datetime.today().strftime('%Y-%m-%d')
+    #my_stream = db.child(datetime).stream(dataListener)
+    #print(type(my_stream))
 
     return HttpResponse(str(data), content_type="text/event-stream")
+
+def getLiveJSONData(request):
+    data = retrieveAllData(db)
+
+    return HttpResponse(str(data), content_type="text/event-stream")
+
+def sse(request):
+    def itero():
+        s = """retry: 2000\n\nevent: message\ndata: gg\n\n"""
+        # data = json.dumps({'time':str(datetime.now())})
+        data = retrieveAllData(db)
+        s = '\n'.join(['retry: 1000', '\n', 'event: message', 'data: %s' % data, '\n'])
+        yield s
+
+        for i in range(10):
+            s = '\n'.join(['retry: 1000', '\n', 'event: message', 'data: %s' % i, '\n'])
+        for i in range(10):
+            s = '\n'.join(['retry: 100000', '\n', 'event: message', 'data: %s' % i, '\n'])
+
+        
+        s = 'data:\n\n'
+        s = ':\n\n'
+
+
+    
+    if request.META.get('HTTP_ACCEPT') == 'text/event-stream':
+        response = StreamingHttpResponse(itero(), content_type="text/event-stream")
+    else:
+        response = HttpResponse(itero(), content_type="text/event-stream") 
+    return   response
 
 def home(request):
 
@@ -59,8 +164,11 @@ def home(request):
 
     currentStation = 'CARRIEDO'
 
+    #threading.Thread(target=loop1_10).start()
+
     targetNumOfCurrentStation = getNumberOfStation(currentStation, context)
 
+    movePassengersToTrain()
 
     return render(request, 'puv/index.html', {'data' : context, 'currentStation' : currentStation, 'numOfCurrentStation' : targetNumOfCurrentStation})
 
