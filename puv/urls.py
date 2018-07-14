7c9c3a15c8d4f7c9be8e6becde0ae33758aacc81
kategwengispert <kggispert@gmail.com>
2018-07-14 22:55:39 +0800

    Added graph

--- puv/urls.py
+++ puv/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-    url(r'^jsonData/$', views.getJSONData, name='getJSONData')
+    url(r'^jsonData/$', views.getJSONData, name='getJSONData'),
+    url(r'^graph/$', views.getGraph, name='getGraph')
 ]
